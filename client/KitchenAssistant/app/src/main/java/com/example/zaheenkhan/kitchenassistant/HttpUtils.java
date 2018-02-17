5ac611e308a866d657b3d06ba9065b6125a233af
Akshay Jain <akshayjain0403@gmail.com>
2018-02-17 17:57:29 -0500

    Perfect UI for inventory management, Adding HttpUtils

--- client/KitchenAssistant/app/src/main/java/com/example/zaheenkhan/kitchenassistant/HttpUtils.java
+++ client/KitchenAssistant/app/src/main/java/com/example/zaheenkhan/kitchenassistant/HttpUtils.java
@@ -1,8 +1,25 @@
 package com.example.zaheenkhan.kitchenassistant;
+import com.loopj.android.http.*;
 
 /**
  * Created by aksha on 2/16/2018.
  */
 
 public class HttpUtils {
+    private static final String BASE_URL = "https://BaseURL";
+
+    private static AsyncHttpClient client = new AsyncHttpClient();
+
+    public static void get(String url, RequestParams params, AsyncHttpResponseHandler responseHandler) {
+        client.get(getAbsoluteUrl(url), params, responseHandler);
+    }
+
+    public static void post(String url, RequestParams params, AsyncHttpResponseHandler responseHandler) {
+        client.post(getAbsoluteUrl(url), params, responseHandler);
+    }
+
+    private static String getAbsoluteUrl(String relativeUrl) {
+        return BASE_URL + relativeUrl;
+    }
+
 }
--- client/KitchenAssistant/app/src/main/java/com/example/zaheenkhan/kitchenassistant/InventoryFragment.java
+++ client/KitchenAssistant/app/src/main/java/com/example/zaheenkhan/kitchenassistant/InventoryFragment.java
@@ -4,20 +4,114 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.text.InputType;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
 
 /**
  * Created by zaheenkhan on 2/13/18.
  */
 
-public class InventoryFragment extends Fragment {
+public class InventoryFragment extends Fragment implements View.OnClickListener{
+
+    ArrayList<Row> rows = new ArrayList<Row>();
+    ArrayList<Item> items = new ArrayList<Item>();
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.activity_inventory,container,false);
+        Button b = (Button) view.findViewById(R.id.b_addItem);
+        b.setOnClickListener(this);
         return view;
+    }
+
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        for(int i=1;i<=3;i++){
+            addRow(getView());
+        }
+    }
+
+    public void addRow(View v){
+        LinearLayout ll = (LinearLayout)getView().findViewById(R.id.ll_itemsList);
+        LinearLayout row = new LinearLayout(getActivity());
+        row.setLayoutParams(new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT));
+        AutoCompleteTextView at = getAutoCompleteTextView();
+        at.setWidth(700);
+        at.setHint("Item");
+        EditText et = new EditText(getActivity());
+        et.setInputType(InputType.TYPE_CLASS_NUMBER);
+        et.setWidth(300);
+        et.setHint("Qty");
+        row.addView(at);
+        row.addView(et);
+        ll.addView(row);
+        rows.add(new Row(at, et));
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()){
+            case R.id.b_addItem:
+                addRow(view);
+                break;
+            case R.id.b_save:
+                save();
+                break;
+        }
+    }
+
+    public void save(){
+        for(Row row: rows){
+            if(row.actv.getText().toString() != null && row.actv.getText().toString() != ""
+                    &&
+                    row.et.getText().toString() != null && row.et.getText().toString() != ""){
+                items.add(new Item(row.actv.getText().toString(), Double.parseDouble(row.et.getText().toString())));
+            }
+        }
+
+    }
+
+    public AutoCompleteTextView getAutoCompleteTextView(){
+        final String[] ITEMS = new String[] {
+                "Milk", "Eggs", "Bread", "Banana", "Oil"
+        };
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(getActivity(),
+                android.R.layout.simple_dropdown_item_1line, ITEMS);
+        final AutoCompleteTextView textView = new AutoCompleteTextView(getActivity());
+        textView.setThreshold(0);
+        textView.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+            @Override
+            public void onFocusChange(View view, boolean b) {
+                textView.showDropDown();
+            }
+        });
+        textView.setAdapter(adapter);
+        return textView;
+    }
+}
+class Row{
+    AutoCompleteTextView actv;
+    EditText et;
+    Row(AutoCompleteTextView p_actv, EditText p_et){
+        actv = p_actv;
+        et = p_et;
+    }
+}
+class Item{
+    String name;
+    double qty;
+    Item(String p_name, double p_qty){
+        name = p_name;
+        qty = p_qty;
     }
 }
